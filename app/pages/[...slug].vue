91b1cb8ccbe36787bed0ffb2f7be84a94d716782
Olsen Kraja <krajaolsen@gmail.com>
2024-12-07 16:09:31 +0100

    Initial commit

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup>
+const router = useRouter()
+</script>
+
+<template>
+  <main>
+    <ContentDoc>
+      <template #not-found>
+        not found
+      </template>
+    </ContentDoc>
+  </main>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post._path">
+      <a :href="post._path">{{ post.title }}</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const {data: posts} = await useAsyncData('posts', () =>
+    queryContent('/posts')
+        .sort({title: 1})
+        .find()
+)
+</script>
